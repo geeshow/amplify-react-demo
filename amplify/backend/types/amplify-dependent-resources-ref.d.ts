51d3280d0ff7820e129415849007f403c25c70c1
kyutae <ken2484@gmail.com>
2023-12-11 18:08:27 +0900

    feat: amplify add storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,20 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "function": {
+    "S3Triggerf41fdb24": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "imageresizer": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
